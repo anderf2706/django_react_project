b38fc1d2a65c5229d348b3dad7a6cdd6b5f2ab38
anderf2706 <anderf2706@gmail.com>
2020-03-02 10:20:07 +0100

    init commit

---
+++ GAE/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GaeConfig(AppConfig):
+    name = 'GAE'
---
+++ url_checker/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import urllib.request
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+@csrf_exempt
+def check_url(request):
+    try:
+        url_status = urllib.request.urlopen(request.body.decode("utf-8")).getcode()
+    except:
+        return HttpResponse(":( Url is Not Working")
+    if (url_status == 200):
+        return HttpResponse("Yey! URL is Working")
+    return HttpResponse(":( Url is Not Working")
+    
